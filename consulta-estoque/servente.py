6422bdb59eb727443f1404ee63005e0e950ba5eb
Giovani Meneguel <34317514+GioAM@users.noreply.github.com>
2020-10-19 22:13:59 -0300

    ajustes

--- consulta-estoque/servente.py
+++ consulta-estoque/servente.py
@@ -1,6 +1,5 @@
 import socket
 import json
-from store import store
 
 HOST = '127.0.0.1'  # Endereco IP do Servidor
 PORT = 5000  # Porta que o Servidor esta
@@ -11,6 +10,22 @@ origin = (HOST, PORT)
 tcp.bind(origin)
 tcp.listen(2)
 con, cliente = tcp.accept()
+
+
+class store():
+    def __init__(self, port, host="127.0.0.1"):
+        self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        dest = (host, port)
+        self.tcp.connect(dest)
+
+    def run(self, data):
+        self.tcp.sendall(bytes(data, encoding="UTF-8"))
+        message_store = self.tcp.recv(1024)
+        message_store = message_store.decode("UTF-8")
+        parsed_data_store = json.loads(message_store)
+        print("Enviando resultados da " + parsed_data_store['store'])
+        data_store = json.dumps(parsed_data_store)
+        con.sendall(bytes(data_store, encoding="UTF-8"))
 
 
 for servidor in servidores:
--- consulta-estoque/servidores/teste.py
+++
@@ -1,7 +0,0 @@
-import csv
-stock = {}
-with open('../estoque/estoque_1.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        stock[row['product_id']] = row['stock']
-print(stock['prod1'])
