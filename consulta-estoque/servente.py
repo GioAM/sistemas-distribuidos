f3e1a47ad4ebe18f85a5be186b0096da876a0b2b
Giovani Meneguel <34317514+GioAM@users.noreply.github.com>
2022-11-16 13:20:45 -0300

    ##

--- consulta-estoque/servente.py
+++ consulta-estoque/servente.py
@@ -1,56 +1,59 @@
 import socket
 import json
+from threading import Thread
 
 HOST = '127.0.0.1'  # Endereco IP do Servidor
 PORT = 5000  # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-stores = []
 servidores = [5001, 5002, 5003]
+stores = []
 origin = (HOST, PORT)
+
 tcp.bind(origin)
 tcp.listen(2)
-con, cliente = tcp.accept()
 
 
 class store():
-    def __init__(self, port, host="127.0.0.1"):
+    def __init__(self, port, host='127.0.0.1'):
+        Thread.__init__(self)
+        self.port = port
+        self.host = host
         self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        dest = (host, port)
+        dest = (self.host, self.port)
         self.tcp.connect(dest)
 
     def run(self, data):
         self.tcp.sendall(bytes(data, encoding="UTF-8"))
-        message_store = self.tcp.recv(1024)
-        message_store = message_store.decode("UTF-8")
-        parsed_data_store = json.loads(message_store)
-        print("Enviando resultados da " + parsed_data_store['store'])
-        data_store = json.dumps(parsed_data_store)
-        con.sendall(bytes(data_store, encoding="UTF-8"))
+        message_com = self.tcp.recv(1024)
+        message_com = message_com.decode("UTF-8")
+        con.sendall(bytes(message_com, encoding="UTF-8"))
+        parsed_data_com = json.loads(message_com)
+        print(parsed_data_com)
 
+
+con, cliente = tcp.accept()
 
 for servidor in servidores:
-    new_store = store(servidor)
-    stores.append(new_store)
-
+    store_server = store(servidor)
+    stores.append(store_server)
 
 while True:
     message_original = con.recv(1024)
     message = message_original.decode("UTF-8")
     parsed_data = json.loads(message)
+    stores_total = {
+        "stores": len(servidores)
+    }
+    stores_total_json = json.dumps(stores_total)
+    con.sendall(bytes(stores_total_json, encoding="UTF-8"))
+    print("Recebendo requisição de cotação")
     message_ids = {
         "product": parsed_data['product']
     }
-    total_stores = {
-        'total_stores': len(stores)
-    }
+    print(message_ids)
     data = json.dumps(message_ids)
-    print("Consultando estoque das lojas...")
-
-    data_stores = json.dumps(total_stores)
-    con.sendall(bytes(data_stores, encoding="UTF-8"))
-
+    print("Resultados:")
     for store_item in stores:
         store_item.run(data)
-
-    print('')
+    stores = []
 
